b2ba9f5e84d861f764ad7c826e5462dc421c50d1
Willian Moreira Finamore <wmfinamore@hotmail.com>
2023-06-03 22:32:16 -0300

    Implemented and migrated the model Activity

--- activities/models.py
+++ activities/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Activity(models.Model):
+    activity = models.CharField(max_length=100)
+    accessibility = models.FloatField()
+    type = models.CharField(max_length=100)
+    participants = models.IntegerField()
+    price = models.FloatField()
+    link = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.activity
